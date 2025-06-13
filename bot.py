81a1fbd989ca1198367dd9bc08e7680cb8aedf5f
Damir <110159540+damirwzrd@users.noreply.github.com>
2025-06-13 15:18:29 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def pay(update, context):
     title = "FreedomPay Тест"
     description = "Оплата товара"
     payload = "custom_payload"
-    provider_token = "6450350554:LIVE:553348"
+    provider_token = "6586749549:LIVE:551874"
     currency = "KGS"
     price = 30
 
