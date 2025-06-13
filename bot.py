594e2b8ac4b17d2c6d890397d102e119ef2fc429
Damir <110159540+damirwzrd@users.noreply.github.com>
2025-06-13 16:11:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ def pay(update, context):
     payload = "custom_payload"
     provider_token = "6450350554:LIVE:546523"
     currency = "KGS"
-    price = 100
+    price = 10
 
     prices = [LabeledPrice("Товар", price * 100)]
 
